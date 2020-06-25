8e2259f9dc3462fc315a3b582c1c8b547ab36fb3
Thomas Peetz <thomas.peetz@ingenieurbuero-peetz.de>
2020-06-25 10:55:04 +0200

    Add build system and command config (#5)
    * Add build system and command config
    
    * Create go.yml
    
    * Add coverage files to .gitignore
    
    * Fix name for binary in build system
    
    * Fix go.yml

---
+++ cmd/root.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	// TaskwarriorConfiguration defines the parameter to disable the config confirmation
+	TaskwarriorConfiguration = "rc.confirmation=off"
+)
+
+var (
+	cfgFile string
+	// Verbose is set, when verbose on the commandline is given.
+	Verbose bool
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "pull-task",
+		Short: "pull-task loads issues from Github, Gitlab and Redmine",
+		Long:  `pull-task loads issues from Github, Gitlab and Redmine and synchronizes with TaskWarrior`,
+	}
+)
+
+// Execute sets the version of ibtp-gitlab abd executes the command.
+func Execute(version string) {
+	rootCmd.Version = version
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/pull-task.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.SetConfigName("pull-task")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("$HOME/.config")
+	}
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		if Verbose {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tpeetz/pull-task
+
+go 1.12
+
+require (
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
+)
